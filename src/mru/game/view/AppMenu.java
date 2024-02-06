c2303e66a02dd41a699e427bfb1a19efe2a76fc7
ellicrw <150177087+ellicrw@users.noreply.github.com>
2024-02-06 14:17:48 -0700

    Update AppMenu.java

--- src/mru/game/view/AppMenu.java
+++ src/mru/game/view/AppMenu.java
@@ -8,3 +8,9 @@ public class AppMenu {
 	 * It also prompts the user for the inputs and validates them 
 	 */
 }
+public static void MainMenu() {
+	System.out.println("Select one of these options:");
+	System.out.println("\t" + "\t" + "(P) Play Game");
+	System.out.println("\t" + "\t" + "(S) Search");
+	System.out.println("\t" + "\t" + "(E) Exit");
+}
